d6c1f9e46304cbb5a98869396ef27008bdcd3d0f
Viannaiv <viva95@windowslive.com>
2018-09-15 18:57:03 +0300

    tested some methods in models/brewery.rb

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,3 +1,16 @@
 class Brewery < ApplicationRecord
     has_many :beers
+
+    # a test method from the material of week 2: probably to be deleted later
+    def print_report
+        puts name
+        puts "established at year #{year}"
+        puts "number of beers #{beers.count}"
+    end
+
+    # another test method for week 2
+    def restart
+        self.year = 2018
+        puts "changed year to #{year}"
+    end
 end
