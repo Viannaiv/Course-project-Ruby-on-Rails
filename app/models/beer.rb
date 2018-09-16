4b3c1ec55aa1844d16cf430d069234c9f10ed255
Viannaiv <viva95@windowslive.com>
2018-09-16 15:04:50 +0300

    added the possibility of creating new ratings

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -5,4 +5,8 @@ class Beer < ApplicationRecord
     def average_rating
         return self.ratings.average(:score).to_s
     end
+
+    def to_s
+        return "#{name}, #{brewery.name}"
+    end
 end
