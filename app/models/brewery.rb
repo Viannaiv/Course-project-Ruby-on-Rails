ec8deb7d05f87b138654c7f54b34b6373b602928
Viannaiv <viva95@windowslive.com>
2018-09-22 19:39:10 +0300

    Removed unnecessary lines, compöeted week 2 ex 16 and modified again a line to get the app back to working in windows localhost

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -4,17 +4,4 @@ class Brewery < ApplicationRecord
     has_many :beers, dependent: :destroy
     has_many :ratings, through: :beers
 
-    # a test method from the material of week 2: probably to be deleted later
-    def print_report
-        puts name
-        puts "established at year #{year}"
-        puts "number of beers #{beers.count}"
-    end
-
-    # another test method for week 2: delete later
-    def restart
-        self.year = 2018
-        puts "changed year to #{year}"
-    end
-
 end
